5aaf9bd10447f2b9ce3465a5ba7a5622bdc987cb
elxro <monetaaaaas@gmail.com>
2023-11-28 21:24:15 -0300

    viendo de agregar la animacion

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -3,7 +3,6 @@ import { FaReact, FaHtml5, FaSass, FaCss3Alt, FaJava } from "react-icons/fa";
 import { GrMysql } from "react-icons/gr";
 import { InfinitMockup, ClinicaMockup, OdontologosMockup } from "../assets/Mockups";
 import Wave from "../assets/wave.png";
-import Line from "../assets/line.svg";
 import TextAnimation from "./TextAnimation";
 import { motion } from "framer-motion"
 
@@ -18,7 +17,25 @@ export default function Proyects() {
     return (
         <>
             <div className="w-full bg-primary relative -top-2 font-inter pt-10">
-                <img src={Line} className="w-full"></img>
+            <svg className="w-full h-full" viewBox="0 80 258 45">
+                <motion.path
+                    fill="transparent"
+                    strokeWidth="3"
+                    stroke="#FBEEE4"
+                    d="M 0 100 Q 53 50 100 100 T 186 97 C 192 86 168 83 173 95 C 177 104 190 116 195 115 C 229 118 222 79 258 100"
+                    animate={{
+                    pathLength: [0, 1],
+                    strokeDashoffset: [2, 0]
+                    }}
+                    transition={{
+                    duration: 2,
+                    ease: "easeInOut",
+                    times: [0, 1],
+                    loop: Infinity,
+                    repeatDelay: 1
+                    }}
+                                    />
+                </svg>
                 <motion.div className="flex flex-col items-center" name='proyectos' >
 
                     <h1 className="font-bold text-secondary 3xl:text-8xl xl:text-7xl lg:text-6xl md:text-5xl text-4xl my-28" > <TextAnimation text='PROYECTOS'/></h1>
