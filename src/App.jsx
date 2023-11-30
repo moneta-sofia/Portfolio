4e414539401b372fc80c354fc5547b0e4437cbe4
elxro <monetaaaaas@gmail.com>
2023-11-29 22:16:24 -0300

    adding loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 
+import { useEffect, useState } from 'react'
 import About from './components/About'
 import Contact from './components/Contact'
+import Loader from './components/Loader'
 import Navbar from './components/Navbar'
 import Proyects from './components/Proyects'
 import Start from './components/Start'
 
+
 function App() {
+  const [cargando, setCargando] = useState(true);
+  
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setCargando(false);
+    });
+  }, []);
+
+  /* -------------- Para evitar que oneko cargue junto al loader -------------- */
+  useEffect(() => {
+    if (window.innerWidth >= 1280  && cargando === false) {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = './oneko.js';
+      document.body.appendChild(scriptElement);
+
+      return () => {
+        document.body.removeChild(scriptElement);
+      };
+    }
+  },[cargando])
+
   return (
+  <>
+    {cargando? 
+    <Loader></Loader> 
+    :
     <div className='flex flex-col justify-center items-center overflow-x-hidden'>
       <Navbar/>
       <Start/>
@@ -17,6 +45,9 @@ function App() {
         <p className='p-5 font-park text-center opacity-70 sm2:text'>©2023 Sofia Moneta.<br></br>Todos los derechos reservados.</p>
       </div>
     </div>
+    }
+  </>
+
   )
 }
 
--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -7,66 +7,66 @@ import TextAnimation from "./TextAnimation";
 import { motion } from "framer-motion"
 import { useInView } from "react-intersection-observer";
 
-    const proyects = [
-        {link:'https://infinit-ecommerce.vercel.app/', name:'Infinit', description:'Proyecto grupal: Alquiler de autos de lujo',icons: [<FaReact title="React"/>, <SiTailwindcss title="Tailwind" />,<SiNextdotjs title="Next" />,<SiPrisma title="Prisma" />, <GrMysql title="mySQL" />], image: InfinitMockup, color: 'bg-gray-200', textColor:'#26485f' },
-        {link:'https://odontologos-sofi.netlify.app/', name:'Odontologos', description:'Proyecto FrontEnd: Listado de Odontologos',icons: [<FaCss3Alt title="CSS" />,<FaSass title="Sass" />,<FaReact title="React" />, <SiVite title="Vite" />], image: OdontologosMockup, color: 'bg-red-100', textColor:'#d02a2e' },
-        {link:'https://github.com/moneta-sofia/ProyectBackend', name:'Clínica', description:'Proyecto Backend: Gestión de pacientes',icons: [<FaHtml5 title="HTML" />,<FaCss3Alt title="CSS" />,<FaSass title="Sass" />,<FaJava title="Java" />,<SiSpring title="SpringBoot" />], image: ClinicaMockup, color: 'bg-slate-100', textColor:'#74a5d2' },
-    ]
+const proyects = [
+    { link: 'https://infinit-ecommerce.vercel.app/', name: 'Infinit', description: 'Proyecto grupal: Alquiler de autos de lujo', icons: [<FaReact title="React" />, <SiTailwindcss title="Tailwind" />, <SiNextdotjs title="Next" />, <SiPrisma title="Prisma" />, <GrMysql title="mySQL" />], image: InfinitMockup, color: 'bg-gray-200', textColor: '#26485f' },
+    { link: 'https://odontologos-sofi.netlify.app/', name: 'Odontologos', description: 'Proyecto FrontEnd: Listado de Odontologos', icons: [<FaCss3Alt title="CSS" />, <FaSass title="Sass" />, <FaReact title="React" />, <SiVite title="Vite" />], image: OdontologosMockup, color: 'bg-red-100', textColor: '#d02a2e' },
+    { link: 'https://github.com/moneta-sofia/ProyectBackend', name: 'Clínica', description: 'Proyecto Backend: Gestión de pacientes', icons: [<FaHtml5 title="HTML" />, <FaCss3Alt title="CSS" />, <FaSass title="Sass" />, <FaJava title="Java" />, <SiSpring title="SpringBoot" />], image: ClinicaMockup, color: 'bg-slate-100', textColor: '#74a5d2' },
+]
 
 
 export default function Proyects() {
     const [ref, inView] = useInView({
         triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
-      });
-    
+    });
+
     return (
         <>
             <div className="w-full bg-primary relative -top-2 font-inter pt-10">
 
-            <svg className="w-full h-full"  ref={ref} viewBox="1 0 190 70">
-                <motion.path
-                    fill="transparent"
-                    strokeWidth="3"
-                    stroke="#FBEEE4"
-                    d="m 0 30 q 39.75 -37.5 75 0 t 64.5 -2.25 c 4.5 -8.25 -13.5 -10.5 -9.75 -1.5 c 3 6.75 12.75 15.75 16.5 15 c 25.5 2.25 20.25 -27 47.25 -11.25"
-                    animate={
-                        inView ? {
-                            pathLength: [0, 1],
-                            strokeDashoffset: [2]
-                          } : {}
-                    }
-                    transition={{
-                    duration: 2,
-                    ease: "easeInOut",
-                    times: [0, 1],
-                    loop: Infinity,
-                    repeatDelay: 1
-                    }}
-                                    />
+                <svg className="w-full h-full" ref={ref} viewBox="1 0 190 70">
+                    <motion.path
+                        fill="transparent"
+                        strokeWidth="3"
+                        stroke="#FBEEE4"
+                        d="m 0 30 q 39.75 -37.5 75 0 t 64.5 -2.25 c 4.5 -8.25 -13.5 -10.5 -9.75 -1.5 c 3 6.75 12.75 15.75 16.5 15 c 25.5 2.25 20.25 -27 47.25 -11.25"
+                        animate={
+                            inView ? {
+                                pathLength: [0, 1],
+                                strokeDashoffset: [2]
+                            } : {}
+                        }
+                        transition={{
+                            duration: 2,
+                            ease: "easeInOut",
+                            times: [0, 1],
+                            loop: Infinity,
+                            repeatDelay: 1
+                        }}
+                    />
                 </svg>
                 <motion.div className="flex flex-col items-center" name='proyectos' >
 
-                    <h1 className="font-bold text-secondary 3xl:text-8xl xl:text-7xl lg:text-6xl md:text-5xl text-4xl my-28" > <TextAnimation text='PROYECTOS'/></h1>
+                    <h1 className="font-bold text-secondary 3xl:text-8xl xl:text-7xl lg:text-6xl md:text-5xl text-4xl my-28" > <TextAnimation text='PROYECTOS' /></h1>
                     <motion.div className="w-full flex flex-row flex-wrap justify-center items-center" >
 
-                        {proyects.map((proy, index) =>{
-                            return(
+                        {proyects.map((proy, index) => {
+                            return (
                                 <motion.a href={proy.link} target="_blank" className={`card-p1 flex flex-col ${proy.color} hover:bg-white px-6 pt-8 mb-16 w-80 overflow-hidden rounded-xl hover:scale-105 transition ease-out shadow-special hover:shadow-special2 mx-5`} key={index}
-                                initial={{ opacity: 0, scale: 0 }}
-                                whileInView={{ opacity: 1, scale: 1, transition: { delay: window.innerWidth <= 768 ? 0.3: 0.3 * index}}}
-                                viewport={{once:true}}>
-                                    
-                                    <h1 className="md:text-4xl text-3xl font-extrabold md:mb-5 mb-2" style={{color:proy.textColor}}>{proy.name}</h1>
+                                    initial={{ opacity: 0, scale: 0 }}
+                                    whileInView={{ opacity: 1, scale: 1, transition: { delay: window.innerWidth <= 768 ? 0.3 : 0.3 * index } }}
+                                    viewport={{ once: true }}>
+
+                                    <h1 className="md:text-4xl text-3xl font-extrabold md:mb-5 mb-2" style={{ color: proy.textColor }}>{proy.name}</h1>
                                     <p className="my-1 font-medium">{proy.description}</p>
                                     <div className="flex justify-between items-center text-3xl px-3 my-5">
-                                        {proy.icons.map((icon)=>{return icon})}
+                                        {proy.icons.map((icon) => { return icon })}
                                     </div>
                                     <img src={proy.image} className=" relative -bottom-5 self-center"></img>
                                 </motion.a>
                             )
-                            })}
+                        })}
                     </motion.div>
-                
+
                 </motion.div>
             </div>
             <img src={Wave} className="w-full rotate-180 relative -top-5"></img>
