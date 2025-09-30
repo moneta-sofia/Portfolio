e86a94324c52565b5d52a59d28de17e35227c2fe
elxrojo <elxrojo115@gmail.com>
2025-09-30 20:41:06 +0200

    feat(close): functional button for close modal

--- src/components/ProyectInfo.jsx
+++ src/components/ProyectInfo.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react"
 import { LanguageContext } from "../contexts/LanguageContext"
 import { IoClose } from "react-icons/io5";
 
-export default function ProyectInfo({proyect}) {
+export default function ProyectInfo({proyect, setOpenInfo, openInfo}) {
     const {isSpanish} = useContext(LanguageContext)
+
     return(
-        <div className={`fixed mx-14 my-10 px-10 py-7 flex flex-col items-center inset-0 z-50 ${proyect.color} rounded-xl shadow-xl`}>
-            <IoClose className="absolute md:right-7 right-5 text-4xl"/>
+        <div className={`${openInfo? 'fixed' : 'hidden'} mx-14 my-10 px-10 py-7 flex flex-col items-center inset-0 z-50 ${proyect.color} rounded-xl shadow-xl`}>
+            <IoClose className="cursor-pointer absolute md:right-7 right-5 text-4xl" onClick={()=>setOpenInfo(false)}/>
             <h1 className={`text-3xl md:pt-0 pt-12 font-bold ${proyect.textColor}`}>{isSpanish? proyect.nameSpanish : proyect.nameEnglish}</h1>
             <p className="md:py-8 py-6">{isSpanish? proyect.descriptionLongSpanish : proyect.descriptionLongEnglish }</p>
         </div>
