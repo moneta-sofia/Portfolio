6567d244ca5684b70bc21ba7cc11d0b7a26ad052
elxrojo <elxrojo115@gmail.com>
2025-09-30 20:10:48 +0200

    add button

--- src/components/ProyectInfo.jsx
+++ src/components/ProyectInfo.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react"
 import { LanguageContext } from "../contexts/LanguageContext"
+import { IoClose } from "react-icons/io5";
 
 export default function ProyectInfo({proyect}) {
     const {isSpanish} = useContext(LanguageContext)
     return(
         <div className={`fixed mx-14 my-10 px-10 py-7 flex flex-col items-center inset-0 z-50 ${proyect.color} rounded-xl shadow-xl`}>
-            <h1 className={`text-3xl font-bold ${proyect.textColor}`}>{isSpanish? proyect.nameSpanish : proyect.nameEnglish}</h1>
-            <p className="py-8">{isSpanish? proyect.descriptionLongSpanish : proyect.descriptionLongEnglish }</p>
+            <IoClose className="absolute md:right-7 right-5 text-4xl"/>
+            <h1 className={`text-3xl md:pt-0 pt-12 font-bold ${proyect.textColor}`}>{isSpanish? proyect.nameSpanish : proyect.nameEnglish}</h1>
+            <p className="md:py-8 py-6">{isSpanish? proyect.descriptionLongSpanish : proyect.descriptionLongEnglish }</p>
         </div>
     )
 }
