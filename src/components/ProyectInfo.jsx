83a5d709a92e62ac1097fa0d24b2fb6446c1290c
elxrojo <elxrojo115@gmail.com>
2025-10-02 00:31:00 +0200

    style: add blur background

--- src/components/ProyectInfo.jsx
+++ src/components/ProyectInfo.jsx
@@ -6,10 +6,13 @@ export default function ProyectInfo({proyect, setOpenInfo, openInfo}) {
     const {isSpanish} = useContext(LanguageContext)
 
     return(
-        <div className={`${openInfo? 'fixed' : 'hidden'} mx-14 my-10 px-10 py-7 flex flex-col items-center inset-0 z-50 ${proyect.color} rounded-xl shadow-xl`}>
-            <IoClose className="cursor-pointer absolute md:right-7 right-5 text-4xl" onClick={()=>setOpenInfo(false)}/>
-            <h1 className={`md:text-4xl text-3xl md:pt-0 pt-12 font-bold `} style={{'color': proyect.textColor}}>{isSpanish? proyect.nameSpanish : proyect.nameEnglish}</h1>
-            <p className="md:py-7 py-6">{isSpanish? proyect.descriptionLongSpanish : proyect.descriptionLongEnglish }</p>
-        </div>
+        <>
+            <div className={`${openInfo? 'fixed' : 'hidden'} z-40 blur inset-0`}/>
+            <div className={`${openInfo? 'fixed' : 'hidden'} mx-14 my-10 px-10 py-7 flex flex-col items-center inset-0 z-50 ${proyect.color} rounded-xl shadow-xl`}>
+                <IoClose className="cursor-pointer absolute md:right-7 right-5 text-4xl" onClick={()=>setOpenInfo(false)}/>
+                <h1 className={`md:text-4xl text-3xl md:pt-0 pt-12 font-bold `} style={{'color': proyect.textColor}}>{isSpanish? proyect.nameSpanish : proyect.nameEnglish}</h1>
+                <p className="md:py-7 py-6">{isSpanish? proyect.descriptionLongSpanish : proyect.descriptionLongEnglish }</p>
+            </div>
+        </>
     )
 }
