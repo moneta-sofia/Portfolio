e07f1d90be3d5bd0473c6a9b7558607de7f4ef6d
elxrojo <elxrojo115@gmail.com>
2025-10-23 15:49:00 +0200

    fix(close modal): styles changed

--- src/components/ProyectInfo.jsx
+++ src/components/ProyectInfo.jsx
@@ -7,9 +7,9 @@ export default function ProyectInfo({proyect, setOpenInfo, openInfo}) {
 
     return(
         <>
-            <div className={`${openInfo? 'fixed' : 'hidden'} z-40 blur inset-0`}/>
+            <div className={`${openInfo? 'fixed' : 'hidden'} z-50 blur inset-0`}/>
             <div className={`${openInfo? 'fixed' : 'hidden'} overflow-auto mx-14 my-10 px-10 py-7 flex flex-col items-center inset-0 z-50 ${proyect.color} rounded-xl shadow-xl`}>
-                <IoClose className="cursor-pointer absolute md:right-7 right-5 text-4xl" onClick={()=>setOpenInfo(false)}/>
+                <IoClose className="cursor-pointer fixed right-0 -my-4 mx-16 text-2xl" onClick={()=>setOpenInfo(false)}/>
                 <h1 className={`md:text-4xl text-3xl md:pt-0 pt-12 text-center font-bold `} style={{'color': proyect.textColor}}>{isSpanish? proyect.nameSpanish : proyect.nameEnglish}</h1>
                 <p className="md:py-7 py-6 md:text-left text-center">{isSpanish? proyect.descriptionLongSpanish : proyect.descriptionLongEnglish }</p>
                 <div className="w-full flex sm:flex-row flex-col items-center justify-between gap-x-5">
